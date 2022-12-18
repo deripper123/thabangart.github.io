e2d94e8ff969b46caffadd2d65b329b7236b9456
tiisetso250 <fomionline44@gmail.com>
2022-12-18 09:28:51 +0000

    add new files

---
+++ food.js
@@ -0,0 +1,16 @@
+// add cart
+
+
+var addItemId = 0;
+
+function addToCart(item) {
+    addItemId += 1;
+    var selectedItem = document.createElement('div');
+    selectedItem.classList.add('image');
+    selectedItem.setAttribute('id', addItemId);
+    var img = document.createElement('img');
+    img.setAttribute('src', item.children[0].currentSrc);
+    var cartItems = document.getElementById('title');
+    selectedItem.append(img);
+    cartItems.append(selectedItem);
+}
